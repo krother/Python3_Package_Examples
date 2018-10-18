3cfe965de7555c3d12e1c6c739f459f598a2be7f
Kristian Rother <krother@academis.eu>
2018-10-18 21:39:39 +0200

    small mod

--- data_analysis/R_from_Python/chi_square.py
+++ data_analysis/R_from_Python/chi_square.py
@@ -1,7 +1,9 @@
 import rpy2.robjects as ro
+
 r = ro.r
 T = r("read.table('csq_input.txt', header=TRUE, sep='\t')")
-#print r.names(T)
-cont_table = r.table(T[1],T[2])
+
+cont_table = r.table(T[1], T[2])
 chitest = r['chisq.test']
-print chitest(T[1],T[2])
+
+print chitest(T[1], T[2])
--- graphics/pillow_carrots/step07-full_image.py
+++ graphics/pillow_carrots/step07-full_image.py
@@ -4,10 +4,10 @@ from PIL import Image, ImageDraw, ImageChops, ImageFont
 img = Image.new('RGBA', (800, 500), "white")
 
 # load starting bitmaps
-carrot = Image.open('carrot.png')
+logo = Image.open('python_logo.png')
 brain = Image.open('brain.png')
 
-carrot = carrot.resize((250, 250))
+logo = logo.resize((150, 150))
 
 # draw an arrow
 arrow = Image.new('RGBA', (250, 250), "white")
@@ -26,14 +26,14 @@ for x in range(250):
 arrow = ImageChops.screen(arrow, gradient)
 
 # combine all component images
-img.paste(carrot, (100, 80))
+img.paste(logo, (100, 147))
 img.paste(arrow, (325, 150))
 img.paste(brain, (500, 100))
 
 # add text
 draw = ImageDraw.Draw(img)
 arial = ImageFont.truetype('arial.ttf', 40)
-draw.text((220,360),"Carrots rock your brain!",fill=(0,0,0),font=arial)
+draw.text((220,360),"Python rocks your brain!",fill=(0,0,0),font=arial)
 
 img.save('step07_final.png')
 
