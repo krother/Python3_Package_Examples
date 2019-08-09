86f6ec09bed8093a339ecc75750e31e746e159a0
Kristian Rother <kristian@spiced-academy.com>
2019-08-10 00:04:54 +0200

    add vader

---
+++ vader/vader.py
@@ -0,0 +1,8 @@
+# pip install vaderSentiment
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+s  = SentimentIntensityAnalyzer()
+
+print(s.polarity_scores("today is a great day to code!"))
+print(s.polarity_scores("syntax errors really suck"))
