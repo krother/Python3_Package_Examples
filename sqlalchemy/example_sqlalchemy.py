5c71752ab8f45fcf1567984dc252202677168e53
Kristian Rother <kristian@spiced-academy.com>
2024-09-15 23:09:49 +0200

    update SQLAlchemy to 2.0

--- sqlalchemy/example_sqlalchemy.py
+++ sqlalchemy/example_sqlalchemy.py
@@ -1,20 +1,28 @@
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 # connection string - specifies
-db = create_engine("sqlite:///example.db")
+engine = create_engine("sqlite:///example.db", echo=False)
 
-db.execute('''
-CREATE TABLE IF NOT EXISTS person (
-    id INTEGER,
-    name VARCHAR(32),
-    description TEXT
-);''')
+with engine.connect() as conn:
+    
+    # write data
+    conn.execute(text('''
+    CREATE TABLE IF NOT EXISTS person (
+        id INTEGER,
+        name VARCHAR(32),
+        description TEXT
+    );'''))
 
-db.execute('INSERT INTO person VALUES (?,?,?)', 1, "Hamlet", "the prince of Denkmark")
-db.execute('INSERT INTO person VALUES (?,?,?)', 2, "Kunigunde", "the queen of Denkmark")
+    conn.execute(text('INSERT INTO person VALUES (:id,:name,:role)'),
+                      [
+                          {"id": 1, "name": "Hamlet", "role": "the prince of Denkmark"},
+                          {"id": 2, "name": "Kunigunde", "role": "the queen of Denkmark"},
+                       ])
+    conn.commit()  # writes to DB
 
-# read data
-result = db.execute('SELECT name, description FROM person')
-for name, description in result:
-    print(name, description)
+    # read data
+    result = conn.execute(text('SELECT name, description FROM person'))
+    for name, description in result:
+        print(name, description)
+    
